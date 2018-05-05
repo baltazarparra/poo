01f90e7b0e8e50b970b25b0e80e203280bc53071
baltazarparra <baltazarparra@outlook.com>
2018-05-05 15:52:39 -0300

    Criando minha primeira class

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
     require_once("config.php");
+    class Pessoa {
+        public $nome;
+        public $descricao;
+        public function falar() {
+            return "<p>Meu nome é ".$this->nome.". ".$this->descricao."</p>";
+        }
+    }
+
+    $levi = new Pessoa();
+    $levi->nome = "<b>Leviathan</b>";
+    $levi->descricao = "Representante do quinto pecado, a Inveja, também sendo tratado como um dos sete príncipes infernais.";
+    echo $levi->falar();
 ?>
