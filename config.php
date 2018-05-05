7071cf4e621e94126474cc5b92a261f550d9fa7b
baltazarparra <baltazarparra@outlook.com>
2018-05-05 15:39:13 -0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+    setlocale(LC_ALL, NULL);
+    setlocale(LC_ALL, 'pt_BR.utf-8');
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    session_start();
+?>
